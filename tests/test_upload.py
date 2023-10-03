53bcd066f4454aae685c59459e4d9908b90008a3
themnsavage <mnsavage@crimson.ua.edu>
2023-10-03 17:08:34 -0500

    fix: do not check if encoded base64 due to possible padding issue that will be fix in lambda

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -7,7 +7,6 @@ from api.upload.post import ensure_base64_padding
 def test_post_handler_success(mock_extract_text):
     encoded_pdf = base64.b64encode(b"dummy PDF content").decode()
     event = {
-        "isBase64Encoded": True,
         "body": encoded_pdf
     }
     mock_extract_text.return_value = "Extracted text from PDF."
@@ -17,17 +16,6 @@ def test_post_handler_success(mock_extract_text):
     assert response['statusCode'] == 200
     assert "PDF processed successfully." in response['body']
     assert "Extracted text from PDF." in response['body']
-
-def test_post_handler_no_base64():
-    event = {
-        "isBase64Encoded": False,
-        "body": "not encoded data"
-    }
-
-    response = handler(event, None)
-
-    assert response['statusCode'] == 400
-    assert "Expected the body to be base64 encoded." in response['body']
     
 def test_no_padding_needed():
     assert ensure_base64_padding("YW55IGNhcm5hbCBwbGVhc3VyZQ==") == "YW55IGNhcm5hbCBwbGVhc3VyZQ=="
