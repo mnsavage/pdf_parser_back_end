506da48904c894a5ee9210ec74b40c2202d6a84a
themnsavage <mnsavage@crimson.ua.edu>
2023-10-03 18:09:49 -0500

    refactor: run black lint

--- api/upload/get.py
+++ api/upload/get.py
@@ -8,19 +8,20 @@ def handler(event, context):
             "title": "Page Formatting & Font",
             "requirements": [
                 "Font: Use a standard 12-point font consistently throughout the document, including headings and subheadings, and must be black font including URLs",
-                'No Blank pages in the documents'
-                ],
+                "No Blank pages in the documents",
+            ],
         },
         {
             "title": "Page Order & Section Formatting",
-            "requirements": [
-                "2 double spaces beneath title"
-            ]
-        }
+            "requirements": ["2 double spaces beneath title"],
+        },
     ]
-    
+
     # Prepare the response
-    response = {"message": "Retrieve PDF requirements successfully.", "header": pdf_requirements}
+    response = {
+        "message": "Retrieve PDF requirements successfully.",
+        "header": pdf_requirements,
+    }
 
     return {
         "statusCode": 200,
