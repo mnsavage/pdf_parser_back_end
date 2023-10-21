0e5faaa897ae8815d7a8b13153cd47e58f97c44d
themnsavage <mnsavage@crimson.ua.edu>
2023-10-21 18:27:43 -0500

    feat: helper function that makes api responses

---
+++ tests/test_response_maker.py
@@ -0,0 +1,22 @@
+import json
+from unittest.mock import patch
+from api.response_maker import make_response
+
+def test_make_response():
+    status_code = 200
+    access_control_allow_origin = "domain.cloudfront.net"
+    body = "hello world"
+    expected_response = {
+        "statusCode": status_code,
+        "body": json.dumps(body),
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": access_control_allow_origin,
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE"
+            },
+    }
+    
+    response = make_response(status_code=200, access_control_allow_origin=access_control_allow_origin, body=body)
+    
+    assert response == expected_response
