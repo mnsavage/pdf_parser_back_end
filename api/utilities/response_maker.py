381fb58e8ee630bf2f1d6d6e1f4a61c1fb73292a
themnsavage <mnsavage@crimson.ua.edu>
2023-11-25 01:34:20 -0600

    test: try enable local host

--- api/utilities/response_maker.py
+++ api/utilities/response_maker.py
@@ -7,8 +7,8 @@ def make_response(status_code=404, access_control_allow_origin=None, body=None):
         "body": json.dumps(body),
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "http://localhost:3000",
-            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
             "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE",
         },
     }
