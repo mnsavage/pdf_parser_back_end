898374978d0be9bf8e80d702e5158fa9bdd1b627
themnsavage <mnsavage@crimson.ua.edu>
2023-11-27 21:45:21 -0600

    test: more print and catch

--- api/upload/post.py
+++ api/upload/post.py
@@ -13,49 +13,49 @@ else:
 
 
 def handler(event, context):
-    print(f'event: {event}')
     UUID = str(uuid.uuid4())
     job_name = f"pdf_parser_{UUID}"
     body = json.loads(event.get("body"))
-    print(f'body:{body}')
+    print(f'file name: {body.get("file_name")}')
     print(f'encoded pdf:{body.get("encoded_pdf")}')
+    try:
+        # store encoded pdf in s3 bucket
+        s3_client = boto3.client("s3")
+        s3_client.put_object(
+            Bucket=os.environ.get("S3"), Key=UUID, Body=body.get("encoded_pdf")
+        )
 
-    # store encoded pdf in s3 bucket
-    s3_client = boto3.client("s3")
-    s3_client.put_object(
-        Bucket=os.environ.get("S3"), Key=UUID, Body=body.get("encoded_pdf")
-    )
+        # submit batch job
+        batch_client = boto3.client("batch")
+        batch_client.submit_job(
+            jobName=job_name,
+            jobQueue=os.environ.get("JOB_QUEUE"),
+            jobDefinition=os.environ.get("JOB_DEFINITION"),
+            containerOverrides={
+                "environment": [
+                    {"name": "DYNAMODB_NAME", "value": os.environ.get("DYNAMODB")},
+                    {"name": "DYNAMODB_KEY", "value": UUID},
+                    {"name": "FILE_NAME", "value": body.get("file_name")},
+                ]
+            },
+        )
 
-    # submit batch job
-    batch_client = boto3.client("batch")
-    batch_client.submit_job(
-        jobName=job_name,
-        jobQueue=os.environ.get("JOB_QUEUE"),
-        jobDefinition=os.environ.get("JOB_DEFINITION"),
-        containerOverrides={
-            "environment": [
-                {"name": "DYNAMODB_NAME", "value": os.environ.get("DYNAMODB")},
-                {"name": "DYNAMODB_KEY", "value": UUID},
-                {"name": "FILE_NAME", "value": body.get("file_name")},
-            ]
-        },
-    )
+        # store batch job information
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(os.environ.get("DYNAMODB"))
 
-    # store batch job information
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ.get("DYNAMODB"))
+        item = {
+            "uuid": UUID,
+            "job_status": "submitted",  # what is the status of the batch job
+            "job_output": None,  # this is where the batch job will put its pdf parser output later
+        }
 
-    item = {
-        "uuid": UUID,
-        "job_status": "submitted",  # what is the status of the batch job
-        "job_output": None,  # this is where the batch job will put its pdf parser output later
-    }
+        table.put_item(Item=item)
 
-    table.put_item(Item=item)
-
-    # Prepare the body
-    body = {"message": "PDF parser job successfully submitted.", "UUID": UUID}
-
+        # Prepare the body
+        body = {"message": "PDF parser job successfully submitted.", "UUID": UUID}
+    except Exception as e:
+        make_response(access_control_allow_origin=os.environ.get("CLOUDFRONT_URL"), body=str(e))
     return make_response(
         status_code=200,
         access_control_allow_origin=os.environ.get("CLOUDFRONT_URL"),
